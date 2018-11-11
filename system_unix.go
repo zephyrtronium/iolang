fdd085eb5c42c1e9475e25c39545777757fd249f
Branden J Brown <zephyrtronium@gmail.com>
2018-11-10 23:15:49 -0800

    System platformVersion on Unix, or at least doesn't break on Windows

---
+++ system_unix.go
@@ -0,0 +1,23 @@
+// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
+
+package iolang
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/sys/unix"
+)
+
+// platformVersion is the string to use for the System platformVersion slot. It
+// is declared in each platform-specific file so that a compilation error
+// occurs on any platform on which it is not implemented.
+var platformVersion string
+
+func init() {
+	var uname unix.Utsname
+	if unix.Uname(&uname) == nil {
+		v, r := uname.Version[:], uname.Release[:]
+		platformVersion = fmt.Sprintf("%s.%s", bytes.Trim(v, "\x00"), bytes.Trim(r, "\x00"))
+	}
+	// If uname failed, we don't have anything else to try.
+}
