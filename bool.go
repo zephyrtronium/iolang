d9a0dc4e23772789e5045ddc72000036cbe9df5a
Branden J Brown <zephyrtronium@gmail.com>
2018-12-24 17:55:57 -0500

    don't create extra CFunctions for bool objects

--- bool.go
+++ bool.go
@@ -2,6 +2,7 @@ package iolang
 
 func (vm *VM) initTrue() {
 	vm.True.Protos = []Interface{vm.BaseObject}
+	object := vm.BaseObject.Slots
 	s := vm.NewString("true")
 	vm.True.Slots = Slots{
 		"asSimpleString": s,
@@ -10,26 +11,27 @@ func (vm *VM) initTrue() {
 		"else":           vm.True,
 		"elseif":         vm.True,
 		"ifFalse":        vm.True,
-		"ifTrue":         vm.NewCFunction(ObjectEvalArgAndReturnSelf),
+		"ifTrue":         object["evalArgAndReturnSelf"],
 		"not":            vm.False,
 		"or":             vm.True,
+		"then":           object["evalArgAndReturnNil"],
 		"type":           s,
 	}
-	vm.True.Slots["then"] = vm.True.Slots["ifTrue"]
 	SetSlot(vm.Core, "true", vm.True)
 }
 
 func (vm *VM) initFalse() {
 	vm.False.Protos = []Interface{vm.BaseObject}
+	object := vm.BaseObject.Slots
 	s := vm.NewString("false")
 	vm.False.Slots = Slots{
 		"and":            vm.False,
 		"asSimpleString": s,
 		"asString":       s,
 		"clone":          vm.False,
-		"else":           vm.NewCFunction(ObjectEvalArgAndReturnNil),
-		"elseif":         vm.NewCFunction(ObjectIf),
-		"ifFalse":        vm.NewCFunction(ObjectEvalArgAndReturnSelf),
+		"else":           object["evalArgAndReturnNil"],
+		"elseif":         object["if"],
+		"ifFalse":        object["evalArgAndReturnSelf"],
 		"ifTrue":         vm.False,
 		"isTrue":         vm.False,
 		"not":            vm.True,
@@ -41,6 +43,7 @@ func (vm *VM) initFalse() {
 
 func (vm *VM) initNil() {
 	vm.Nil.Protos = []Interface{vm.BaseObject}
+	object := vm.BaseObject.Slots
 	s := vm.NewString("nil")
 	vm.Nil.Slots = Slots{
 		"and":            vm.False,
@@ -50,8 +53,8 @@ func (vm *VM) initNil() {
 		"clone":          vm.Nil,
 		"else":           vm.Nil,
 		"elseif":         vm.Nil,
-		"ifNil":          vm.NewCFunction(ObjectEvalArgAndReturnSelf),
-		"ifNilEval":      vm.NewCFunction(ObjectEvalArg),
+		"ifNil":          object["evalArgAndReturnSelf"],
+		"ifNilEval":      object["evalArg"],
 		"ifNonNil":       vm.Nil, // TODO: Io calls "Object_thisContext()"
 		"ifNonNilEval":   vm.Nil, // TODO: same
 		"isNil":          vm.True,
