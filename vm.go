2f63b2e1e04b15be8626e2c9d754f650110ec6c1
Branden J Brown <zephyrtronium@gmail.com>
2019-01-19 04:14:45 -0500

    implemented Date today, isToday, secondsToRun

--- vm.go
+++ vm.go
@@ -716,6 +716,24 @@ Map do(
 
 Date do(
 	setSlot("+", method(dur, self clone += dur))
+
+	// The original implementation of Date today starts with Date now, which
+	// means it modifies the Core Date instead of the receiver...
+	today := method(now setHour(0) setMinute(0) setSecond(0))
+	isToday := method(
+		n := Date clone now
+		n year == year and n month == month and n day == day
+	)
+
+	// The only real difference between secondsToRun and cpuSecondsToRun is
+	// that the former relays its stop status, so any control flow in the
+	// message will cause it to quit instead. If we were to make the latter
+	// actually measure CPU time only, it would be a different story.
+	secondsToRun := method(
+		t := Date clone now
+		call evalArgAt(0)
+		t secondsSinceNow
+	) setPassStops(true)
 )
 
 Duration do(
