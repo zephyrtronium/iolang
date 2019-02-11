844095c2abd9d4e04a2aa54dc725471d05c2b1bb
Branden J Brown <zephyrtronium@gmail.com>
2019-02-11 07:45:28 -0500

    implemented OperatorTable reverseAssignOperators

--- vm.go
+++ vm.go
@@ -1015,6 +1015,8 @@ OperatorTable do(
 	addOperator := method(name, prec, operators atPut(name, prec ifNilEval(0)); self)
 	addAssignOperator := method(name, calls, assignOperators atPut(name, calls asSymbol asUTF8); self)
 
+	reverseAssignOperators := method(assignOperators reverseMap)
+
 	asString := method(
 		b := Sequence clone appendSeq(self asSimpleString, ":\nOperators")
 		self operators values unique sort foreach(prec,
