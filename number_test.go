686fe8f578829fb0d49d8510d2663b66ba7dc1b3
Branden J Brown <zephyrtronium@gmail.com>
2020-02-15 13:53:42 -0500

    use separate constraints for TestNumberCache

--- number_test.go
+++ number_test.go
@@ -7,7 +7,15 @@ import (
 // TestNumberCache tests that certain numbers always have identical objects.
 func TestNumberCache(t *testing.T) {
 	vm := TestingVM()
-	for i := numberCacheMin; i <= numberCacheMax; i++ {
+	// These constants are independent from but (as of writing) equal to those
+	// used to construct the number cache. It is allowable for more numbers to
+	// be cached, but fewer is not allowable, as there is real(ish?) code that
+	// depends on caching of certain numbers.
+	const (
+		testNumberCacheMin = -10
+		testNumberCacheMax = 256
+	)
+	for i := testNumberCacheMin; i <= testNumberCacheMax; i++ {
 		if vm.NewNumber(float64(i)) != vm.NewNumber(float64(i)) {
 			t.Error(i, "not cached")
 		}
